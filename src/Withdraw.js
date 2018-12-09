3e93911ee35e90089ab795c57111b36acc4d17f1
Glitch (bleedingeffigy-bday-dapp) <none>
2018-12-09 13:26:09 +0000

    🦁🚋 Checkpoint ./src/Withdraw.js:1934267/287

--- src/Withdraw.js
+++ src/Withdraw.js
@@ -8,7 +8,7 @@ class Withdraw extends React.Component {
   }
 
   render() {
-    return <div>Withdraw Component: {this.drizzleState.accounts[0]}</div>;
+    return <div>Withdraw Component: </div>;
   }
 }
 
