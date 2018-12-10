19cbfed00e6fdb36fabd0f5522fbad3aa8762784
Glitch (bday-dapp) <none>
2018-12-10 09:03:28 +0000

    🙈🏬 Checkpoint ./src/Withdraw.js:1934267/687

--- src/Withdraw.js
+++ src/Withdraw.js
@@ -1,14 +1,21 @@
 import React from "react";
 
 class Withdraw extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      address: null
+    }
+  }
   componentDidMount() {
     const { drizzle, drizzleState } = this.props;
     console.log(drizzle);
     console.log(drizzleState);
+    this.setState({address: drizzleState.accounts[0]});
   }
 
   render() {
-    return <div>Withdraw Component: </div>;
+    return <div>Withdraw Component: {this.state.address}</div>;
   }
 }
 
